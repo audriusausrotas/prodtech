2cfcfb00015bb31aa5d053927fc71b3eec9f23f1
Audrius Ausrotas <audrius.ausrotas@gmail.com>
2023-03-20 23:11:29 +0200

    gallery updated

--- Components/Gallery/GalleryCarousel.js
+++ Components/Gallery/GalleryCarousel.js
@@ -9,26 +9,26 @@ export default function GalleryCarousel({
   prevHandler,
 }) {
   return (
-    <div className="fixed top-0 left-0 z-50 flex w-full h-screen p-20 bg-white select-none ">
+    <div className="fixed top-0 left-0 z-50 flex w-full h-screen bg-white select-none scroll ">
       <img
         src={image}
         alt=""
-        className="relative object-contain h-full m-auto pointer-events-none shadow-s1 rounded-xl"
+        className="relative object-contain h-[70%] m-auto pointer-events-none shadow-s1 rounded-xl"
       />
       <IoIosArrowDropleftCircle
-        size={70}
+        size={60}
         onClick={prevHandler}
-        className="absolute transition-all rounded-full cursor-pointer top-1/2 hover:scale-125 left-24 hover:text-secondary text-primary"
+        className="absolute transition-all rounded-full cursor-pointer top-1/2 hover:scale-110 left-[10%] hover:text-secondary text-gray1"
       />
       <IoIosArrowDroprightCircle
-        size={70}
+        size={60}
         onClick={nextHandler}
-        className="absolute transition-all cursor-pointer hover:scale-125 top-1/2 hover:text-secondary right-24 text-primary"
+        className="absolute transition-all cursor-pointer hover:scale-110 top-1/2 hover:text-secondary right-[10%] text-gray1"
       />
       <IoMdCloseCircle
-        size={70}
+        size={60}
         onClick={closeHandler}
-        className="absolute transition-all rounded-full cursor-pointer hover:text-secondary hover:scale-125 top-10 right-24 text-primary"
+        className="absolute transition-all rounded-full cursor-pointer hover:text-secondary hover:scale-110 top-10 right-10 text-gray1"
       />
       <div className="absolute text-2xl font-bold text-white top-24 left-1/2">
         {image?.slice(-6)}
