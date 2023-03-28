24595265e7459c6befec50e1c93fa3c0dcb4b98c
Audrius Ausrotas <audrius.ausrotas@gmail.com>
2023-03-28 23:15:36 +0300

    logo changed

--- Components/Layout/Navigation/Navigation.js
+++ Components/Layout/Navigation/Navigation.js
@@ -5,7 +5,6 @@ import Link from "next/link";
 import Menu from "./Menu";
 import LanguageSelect from "./LanguageSelect";
 import HamburgerMenu from "./HamburgerMenu";
-
 export default function Navigation() {
   const router = useRouter();
   const [language, setLanguage] = useState("LT");
@@ -30,7 +29,7 @@ export default function Navigation() {
       <div className="max-w-[1440px] h-24 m-auto flex items-center  text-white justify-around ">
         <HamburgerMenu language={language} />
         <Link passHref href="/">
-          <Image src="/logo.png" alt="Prodtech logo" width={200} height={83} />
+          <Image src="/Logo.png" alt="Prodtech logo" width={200} height={83} />
         </Link>
 
         <div className="flex xl:gap-16 lg:gap-8">
