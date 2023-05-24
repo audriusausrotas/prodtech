cb319aebe9155a0f2a974524d9bfeecefe613bb1
Audrius Ausrotas <audrius.ausrotas@gmail.com>
2023-05-24 19:01:09 +0300

    bugg fixed

--- Components/Home/ServicesItem.js
+++ Components/Home/ServicesItem.js
@@ -4,13 +4,13 @@ import Image from "next/image";
 
 export default function ServicesItem({ data }) {
   return (
-    <div className="max-w-[280px] flex flex-col ">
+    <div className="max-w-[280px] flex flex-col w-full">
       <Image
         src={data.item_picture.url}
         alt={data.item_picture.alt}
         width={data.item_picture.dimensions.width}
         height={data.item_picture.dimensions.height}
-        className="object-cover object-center border-2 border-b-0 rounded-t-lg h-96 border-secondary"
+        className="object-cover object-center w-full border-2 border-b-0 rounded-t-lg h-96 border-secondary"
       />
       <Link
         href={data.item_link}
