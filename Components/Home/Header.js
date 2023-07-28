0596680b77478ac636856ff78c4bd696f511685d
Audrius Ausrotas <audrius.ausrotas@gmail.com>
2023-07-28 23:29:19 +0300

    fixing mobile buggs

--- Components/Home/Header.js
+++ Components/Home/Header.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Header({ data }) {
   return (
-    <div className="w-full max-w-[1440px] pt-20 m-auto h-[680px] flex flex-col justify-evenly items-center text-white relative">
+    <div className="w-full max-w-[1440px] pt-20 m-auto h-[680px] flex flex-col justify-evenly items-center text-white relative overflow-hidden">
       <Image
         priority
         src={data.image.url}
@@ -12,7 +12,7 @@ export default function Header({ data }) {
         height={data.image.dimensions.height}
         className="absolute top-0 left-0 object-cover object-center w-full h-full rounded-b-lg"
       />
-      <h1 className="z-10 w-full px-4 text-5xl font-bold leading-snug text-center sm:text-6xl lg:px-40">
+      <h1 className="z-10 w-full px-4 text-5xl font-bold leading-snug text-center sm:text-3xl lg:px-40">
         {data.header}
       </h1>
       <h4 className="z-10 text-2xl font-semibold">{data.text}</h4>
